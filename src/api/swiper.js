7162e5313eb83e6603bed9168e0d5587e5f34878
Irelia <yaokai729@outlook.com>
2021-10-27 23:30:09 +0800

    添加blog页面

--- src/api/swiper.js
+++ src/api/swiper.js
@@ -15,3 +15,4 @@ export function del(params) {
     method: "get"
   });
 }
+
---
+++ src/views/blog/list.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="">博客</div>
+</template>
+
+<script>
+import { fetchList } from "@/api/blog";
+export default {
+  components: {},
+  data() {
+    return {
+      blogList: [],
+      count: 50
+    };
+  },
+  created () {
+    this.getList()
+  },
+  methods: {
+    getList() {
+      fetchList({
+        start: this.blogList.length,
+        count: this.count
+      }).then(res=>{
+        console.log(res);
+      });
+    }
+  }
+};
+</script>
+
+<style lang="" scoped></style>
