a2fa1ef8c8a58eb0b487414c10ed499f4e56e0a6
Irelia <yaokai729@outlook.com>
2021-10-28 23:44:08 +0800

    添加blog数据

---
+++ src/api/blog.js
@@ -0,0 +1,18 @@
+import request from "@/utils/request";
+const baseURL = "http://localhost:3000";
+
+export function fetchList(params) {
+  return request({
+    url: `${baseURL}/blog/list`,
+    method: "get",
+    params: { ...params },
+  });
+}
+
+export function del(params) {
+  return request({
+    url: `${baseURL}/blog/del`,
+    method: "post",
+    params: { ...params }
+  });
+}
